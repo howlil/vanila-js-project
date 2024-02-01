feb48ddf446dccd2ea61d593747656900641cb49
Mhd Ulil Abshar <87646428+uldevjs@users.noreply.github.com>
2024-02-01 13:45:01 +0700

    inisiasi

--- app/todo-app/src/Index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import ToDoApp from './component/ToDoApp';
- 
-// styling
-import './style/index';
- 
-const root = createRoot(document.getElementById('root'));
-root.render(<ToDoApp />);
